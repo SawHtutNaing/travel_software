bfc474d74edb2998d51637d3401a0a02029a8cdc
SawHtutNaing <sawhtutnaing2004@gmail.com>
2025-06-05 23:50:28 +0630

    dd

--- app/Http/Controllers/TourController.php
+++ app/Http/Controllers/TourController.php
@@ -11,7 +11,7 @@ class TourController extends Controller
     public function __construct()
     {
         $this->middleware('auth');
-        $this->middleware('role:admin');
+        // $this->middleware('role:admin');
     }
 
     public function index()
